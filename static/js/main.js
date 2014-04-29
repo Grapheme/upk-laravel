1d8cb822acc4a02f6b5c64fa26d1d16f17e24a6d
vkharseev <vladimir@realitygroup.ru>
2014-04-29 17:40:20 +0400

    Static new

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,34 @@
+function positionitems() {
+  $items = $('.index-block').add('.slide-title').add('.wrapper-abs');
+
+  var time = 200;
+
+  $items.each(function() {
+	  var that = $(this);
+      setTimeout( function(){ that.css({opacity: 1}); }, time);
+      time += 200;
+  });
+}
 $(function(){
 	$('.block-title').each(function(){
 		var pad = ( $('.index-block').width() - $(this).width() ) / 2;
 		$(this).css('padding-left', pad);
 	});
+
+	positionitems();
+});
+
+$('.intranet').click(function(e){
+	e.stopPropagation();
+	$(this).addClass('active');
+	$('.sign-up').addClass('active');
+});
+
+$('.sign-up').click(function(e){
+	e.stopPropagation();
+});
+
+$(document).click(function(){
+	$('.intranet').removeClass('active');
+	$('.sign-up').removeClass('active');
 });
