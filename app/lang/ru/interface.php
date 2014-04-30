0e63ef91d99adf106394fb192202d27521bbb126
vkharseev <vladimir@realitygroup.ru>
2014-04-30 21:29:14 +0400

    Переводы

--- app/lang/ru/interface.php
+++ app/lang/ru/interface.php
@@ -17,4 +17,13 @@ return array(
 	
 	'FOOTER_COPYRIGHT' => 'ООО «Успенский перерабатывающий комплекс»',
 	'FOOTER_MAKEIN' => 'Сделано в <a href="http://grapheme.ru" target="_blank" class="grphm-copy">ГРАФЕМА</a>',
+
+
+	'PAGES_FOR_INVESTORS' => 'Инвесторам',
+
+
+	'FORM_SIGNIN_SECURE_2_HEADER' => 'Авторизация',
+	'FORM_INPUT_PLACEHOLDER_LOGIN' => 'логин',
+	'FORM_INPUT_PLACEHOLDER_PASSWORD' => 'пароль',
+	'FORM_SIGNIN_SUBMIT' => 'Войти',
 );
