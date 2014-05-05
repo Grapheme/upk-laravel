e84d889a0448c46bd4ee36dbbee84a434f63b387
sngrl <reserved@mail.ru>
2014-05-05 15:04:16 +0400

    Структура таблиц для новостей, функция генерации урла

---
+++ app/database/migrations/2014_05_05_133757_create_i18n_news_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateI18nNewsTable extends Migration {
+
+	public function up(){
+		Schema::create('i18n_news', function(Blueprint $table) {
+			$table->increments('id');
+            $table->string('slug',64)->nullable();
+            $table->string('template',100)->nullable();
+            $table->boolean('publication')->default(1)->unsigned()->nullable();
+            $table->boolean('start_page')->default(0)->unsigned()->nullable();
+			$table->timestamps();
+            $table->timestamp('published_at');
+       		$table->index('publication');
+       		$table->index('published_at');
+		});
+
+		Schema::create('i18n_news_meta', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('news_id')->default(0)->unsigned()->nullable();
+            $table->string('language',10)->nullable();
+            $table->string('title',100)->nullable();
+			$table->text('preview')->nullable();
+			$table->mediumText('content')->nullable();
+            $table->string('seo_url',255)->nullable();
+            $table->string('seo_title',255)->nullable();
+            $table->text('seo_description')->nullable();
+            $table->text('seo_keywords')->nullable();
+            $table->string('seo_h1')->nullable();
+			$table->timestamps();
+    		$table->index('news_id');
+    		$table->index('language');
+    		$table->index('seo_url');
+		});
+	}
+
+	public function down(){
+		Schema::drop('i18n_news');
+		Schema::drop('i18n_news_meta');
+	}
+
+}
